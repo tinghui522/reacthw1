04142f3412a46e573b6f6cc8c0881f8b5a4f7394
tinghui522 <55957964+tinghui522@users.noreply.github.com>
2021-03-02 21:50:44 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
+    <div>
     <div>
         <div className="nav-title">
             <a>I'm IU</a>
@@ -22,7 +24,10 @@ function App() {
           <a href="store.html" ><li>Store</li></a>        
           <a href="concert.html" ><li>Concert</li></a>           
         </ul>  
-   
+        <div className="navsquare"></div>
+    <div className="bigIU">
+        <img src="images/1920/组 230@2x.png" className="IMIU"/>
+    </div>
     
         <div className="aboutlyrics">"<br/>
             I’ll be there, 홀로 걷는 너의 뒤에<br/>
@@ -32,34 +37,22 @@ function App() {
             love poem<br/>
             "
         </div>
-        <div className="aboutpages">
-            
-                <p>Name<br/>이지은</p>
-                <p>Birthday<br/>1993.05.16</p>
-                <p>Debut Day<br/>2008.09.18</p>
-
-            
-            
-                <p>Music Style<br/>KPOP.R B</p>
-                <p>Fans name<br/>U-ana</p>
-                <p>Debut Day<br/>2008.09.18</p>
-            
-            
-               
-            
         </div>
-
+<div>
     
-  <footer>
-    <div className="footericon">
-        <a href="https://www.facebook.com/iu.loen/" className="aicon"><img src="images/1920/facebook.png"  className="icon"/></a>
-        <a href="https://www.instagram.com/dlwlrma/" className="aicon"><img src="images/1920/instagram.png" className="icon"/></a>
-        <a href="https://twitter.com/_iuofficial" className="aicon"><img src="images/1920/twitter.png"   className="icon"/></a>
-        <a href="https://www.youtube.com/channel/UC3SyT4_WLHzN7JmHQwKQZww" className="aicon"><img src="images/1920/youtube.png"   className="icon"/></a>
+        <footer>
+          <div className="footericon">
+              <a href="https://www.facebook.com/iu.loen/" className="aicon"><img src="images/1920/facebook.png"  className="icon"/></a>
+              <a href="https://www.instagram.com/dlwlrma/" className="aicon"><img src="images/1920/instagram.png" className="icon"/></a>
+              <a href="https://twitter.com/_iuofficial" className="aicon"><img src="images/1920/twitter.png"   className="icon"/></a>
+              <a href="https://www.youtube.com/channel/UC3SyT4_WLHzN7JmHQwKQZww" className="aicon"><img src="images/1920/youtube.png"   className="icon"/></a>
+          </div>
+          <div className="footerword">©2019-2020 I'M IU.ALL RIGHTS RESERVED. </div>
+    </footer>
     </div>
-    <div className="footerword">©2019-2020 I'M IU.ALL RIGHTS RESERVED. </div>
-</footer>
-</div>
+
+    </div>
+
 
 
 
